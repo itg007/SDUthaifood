ae5179d9fc0fc9e80e3c7d1a992cac73e4e81548
ITG007 <lloll171@hotmail.com>
2019-04-02 22:28:16 +0700

    UI commit

---
+++ app/src/main/java/sdu/cs/sirichai/sduthaifood/MainActivity.java
@@ -0,0 +1,18 @@
+package sdu.cs.sirichai.sduthaifood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //
+        int[] ints = new int[]{R.drawable.logo_01, R.drawable.logo_02, R.drawable.logo_03,
+        R.drawable.logo_04, R.drawable.logo_05, R.drawable.logo_06, R.drawable.logo_07,
+        R.drawable.logo_08, R.drawable.logo_09};
+    }
+}
