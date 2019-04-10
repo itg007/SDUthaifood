13a5efa1b6360c5d9b3c1f1cfbf41735da2b7f2c
ITG007 <lloll171@hotmail.com>
2019-04-10 22:08:55 +0700

    UI Complete

--- app/src/main/java/sdu/cs/sirichai/sduthaifood/MainActivity.java
+++ app/src/main/java/sdu/cs/sirichai/sduthaifood/MainActivity.java
@@ -1,21 +1,24 @@
 package sdu.cs.sirichai.sduthaifood;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit ประกาศตัวแปร
-    Button btn1,btn2,btn3,btn4,btn,btn5,btn6,btn7,btn8,btn9,btn10;
+    ImageButton btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn10;
     int[] ints = new int[]{R.drawable.logo_01, R.drawable.logo_02, R.drawable.logo_03,
             R.drawable.logo_04, R.drawable.logo_05, R.drawable.logo_06, R.drawable.logo_07,
             R.drawable.logo_08, R.drawable.logo_09};
     String[] approachStrings, detailStrings, driveStrings, responsibleStrings;
 
-    @SuppressLint("WrongViewCast")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
         btn9 = findViewById(R.id.btm9);
         btn10 = findViewById(R.id.btm10);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //ส่งข้อมูลไปหน้า Detail
+                Intent detaiIntent = new Intent(MainActivity.this, Detail.class);
+
+                detaiIntent.putExtra("approach", approachStrings);
+                detaiIntent.putExtra("detail", detailStrings);
+                detaiIntent.putExtra("drive", driveStrings);
+                detaiIntent.putExtra("responsible", responsibleStrings);
+
+                startActivity(detaiIntent);
+
+            }
+        });
 
     }//end onCreate
 }//end class
